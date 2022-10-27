28d30ec292b37798bf47eabb41aa580be394ceed
Corentin <cgirard@itroom.fr>
2022-10-27 10:57:40 +0200

    feat(Actions): test success

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: "home")]
+#[Route('/', name: 'home')]
 class HomeController extends AbstractController
 {
-    #[Route('', name: "_index")]
+    #[Route('', name: '_index')]
     public function index(): Response
     {
         return $this->render('front/home.html.twig');
