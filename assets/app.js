17333b538049d82a49de33c71e9c9cdef86c6e99
Corentin <cgirard@itroom.fr>
2022-10-28 12:20:29 +0200

    feat: added bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Tooltip, Toast, Popover } from 'bootstrap'
+
 // start the Stimulus application
 import './bootstrap';
